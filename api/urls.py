70fa7dded29fb34ecef701b6273df10c466113d8
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2025-03-09 21:11:19 +0700

    Add obstacle marking api

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
-from .views import BusStopLocationView, LineSpecailRouteView, LineOneRouteView
+from .views import BusStopLocationView, LineSpecailRouteView, LineOneRouteView, ObstacleMarkerView
 
 urlpatterns = [
     path('line-one/', LineOneRouteView.as_view()),
     path('line-special/', LineSpecailRouteView.as_view()),
     path('bus-stop-location/', BusStopLocationView.as_view()),
+    path('obstacle-marker/<str:obstacle_type>/', ObstacleMarkerView.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
