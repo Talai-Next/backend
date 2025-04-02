960bc5f15f373495ff661c26a510379620cb462b
Nantawat Sukrisunt <nantawat.suk@ku.th>
2025-04-02 15:56:17 +0700

    add station_id filed

--- api/serializers/bus_serializer.py
+++ api/serializers/bus_serializer.py
@@ -4,6 +4,7 @@ class BusSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     bus_id = serializers.CharField()
     line = serializers.CharField()
+    station_id = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     latitude = serializers.FloatField()
     longitude = serializers.FloatField()
     speed = serializers.IntegerField()
